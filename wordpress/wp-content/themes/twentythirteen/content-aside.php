11ddf99e533777b63072a2f5a9cb9a7708ed0cb3
M_Ishikawa <ishikawam@nifty.com>
2013-12-05 03:33:17 +0900

    sync original files

--- wordpress/wp-content/themes/twentythirteen/content-aside.php
+++ wordpress/wp-content/themes/twentythirteen/content-aside.php
@@ -8,7 +8,7 @@
  */
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> >
 	<div class="entry-content">
 		<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentythirteen' ) ); ?>
 		<?php wp_link_pages( array( 'before' => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentythirteen' ) . '</span>', 'after' => '</div>', 'link_before' => '<span>', 'link_after' => '</span>' ) ); ?>
